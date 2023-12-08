e6bb60387fd807a25d91f81f68ba6cc1a1e95ba3
jaimeflneto <jaimeflneto@gmail.com>
2023-12-08 10:34:30 -0300

    React query do connect to api

--- App.tsx
+++ App.tsx
@@ -2,7 +2,18 @@ import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Routes from "./src/routes";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./src/theme";
+
+const queryClient = new QueryClient();
 
 export default function App() {
-  return <Routes />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Routes />
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
 }
